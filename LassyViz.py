5c2cb3722b023bc5fa316314219000ae3ec90b21
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-03-01 16:54:57 +0100

    Add data labels to plot

--- LassyViz.py
+++ LassyViz.py
@@ -13,5 +13,17 @@ df = pd.read_csv(args.csv_input)
 
 sns.set(style="darkgrid")
 ax = sns.countplot(x="country", data=df)
+
+for i, bar in enumerate(ax.patches):
+    h = bar.get_height()
+    ax.text(
+        i, # bar index (x coordinate of text)
+        h+25, # y coordinate of text
+        '{}'.format(int(h)),  # y label
+        ha='center', 
+        va='center', 
+        fontweight='bold', 
+        size=14)
+
 fig = ax.get_figure()
-fig.savefig("out.png") 
+fig.savefig(f"{args.csv_input}.png") 
